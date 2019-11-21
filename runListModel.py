3023bcf5be03386a87ba278b020520ee995863c1
Jarvis <35248101+JarvisUSTC@users.noreply.github.com>
2019-11-21 16:52:18 +0800

    Delete runListModel.py

--- runListModel.py
+++
@@ -1,7 +0,0 @@
-from ListNet import *
-
-ListNetModel = ListNet()
-ListNetModel.ReadExcelFile("output.xls")
-ListNetModel.Learning2Rank()
-ListNetModel.WriteFileModel("weight.xls")
-ListNetModel.PredictRank("scores.xls")
